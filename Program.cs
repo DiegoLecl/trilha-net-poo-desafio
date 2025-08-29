eed012aa309514ea77d37cc7e1677ae2fdc1532b
Diego Leal <diegoclementepb@gmail.com>
2025-08-29 13:40:34 -0300

    Desafio POO concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Teste com Nokia ===");
+        Smartphone nokia = new Nokia("119999999", "Nokia Tijolão", "111111111111", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\n=== Teste com iPhone ===");
+        Smartphone iphone = new Iphone("119888888", "iPhone 14 Pro", "222222222222", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
